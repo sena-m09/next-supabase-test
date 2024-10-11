6a346f32b08ebd7810bcb29cff15f0604bee5557
sena-m09 <sena-murakami@gaji.jp>
2024-10-11 10:28:38 +0900

    add form

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,16 +1,92 @@
 import { currentUser } from "@/app/data/auth";
 import { redirect } from "next/navigation";
 
-export default async function mypage() {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  amount: z.coerce.number().min(1),
+  name: z.string().min(1).max(255),
+});
+
+type FormType = z.infer<typeof formSchema>;
+
+export default async function Mypage() {
   const user = await currentUser();
+
+  const form = useForm<FormType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      amount: 0,
+    }
+  });
 
   if(!user) {
     redirect("/login");
   }
 
+  const onSubmit = () => {
+
+  };
+
   return (
     <>
       <h1>マイページ</h1>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit, () => {
+          alert('error');
+        })} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </>
   );
 }
